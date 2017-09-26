fd472b789f2feb2f7d9376946b0e184d64f85eec
Bunderant <magnenator@gmail.com>
2017-09-25 23:48:24 -0500

    fixed crash on suspend/resume - updated classes.jar from 5.6.2p3, updated UnityARPlayerActivity to inherit from UnityPlayerActivity (UnityPlayerNativeActivity now obsolete), fixed permissions check for API level < 23

--- src/Android_Unity_Player_Source/AndroidStudioProj/UnityARPlayer/src/main/java/org/artoolkit/ar/unity/UnityARPlayerActivity.java
+++ src/Android_Unity_Player_Source/AndroidStudioProj/UnityARPlayer/src/main/java/org/artoolkit/ar/unity/UnityARPlayerActivity.java
@@ -49,8 +49,8 @@
 
 package org.artoolkit.ar.unity;
 
+import com.unity3d.player.UnityPlayerActivity;
 import android.Manifest;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
@@ -61,9 +61,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
 import android.view.WindowManager;
-import android.widget.FrameLayout;
-import com.unity3d.player.UnityPlayerNativeActivity;
-import org.artoolkit.ar.base.camera.CameraPreferencesActivity;
+//import org.artoolkit.ar.base.camera.CameraPreferencesActivity;
 import jp.epson.moverio.bt200.DisplayControl;
 
 //Imports below required to ask for permission to use the camera.
@@ -74,7 +72,7 @@ import android.widget.Toast;
 
 //For Epson Moverio BT-200. BT200Ctrl.jar must be in libs/ folder.
 
-public class UnityARPlayerActivity extends UnityPlayerNativeActivity {
+public class UnityARPlayerActivity extends UnityPlayerActivity {
 
     protected final static String TAG = "UnityARPlayerActivity";
 
@@ -127,12 +125,15 @@ public class UnityARPlayerActivity extends UnityPlayerNativeActivity {
         int resID = getResources().getIdentifier("preferences", "xml", getPackageName());
         PreferenceManager.setDefaultValues(this, resID, false);
 
-		//Request permission to use the camera on android 23+
-        int permissionCheck = ContextCompat.checkSelfPermission(this.getApplicationContext(), Manifest.permission.CAMERA);
-        if (permissionCheck != PackageManager.PERMISSION_GRANTED)
-        {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST_CAMERA);
+        if (Build.VERSION.SDK_INT >= 23) {
+            //Request permission to use the camera on android 23+
+            int permissionCheck = ContextCompat.checkSelfPermission(this.getApplicationContext(), Manifest.permission.CAMERA);
+            if (permissionCheck != PackageManager.PERMISSION_GRANTED)
+            {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST_CAMERA);
+            }
         }
+
     }
 
 	//Handle the result of asking the user for camera permission.
@@ -191,7 +192,8 @@ public class UnityARPlayerActivity extends UnityPlayerNativeActivity {
     }
 
     void launchPreferencesActivity() {
-        startActivity(new Intent(this, CameraPreferencesActivity.class));
+        // Disabled for now, don't want to ever ask users to set this up themselves. 
+        // startActivity(new Intent(this, CameraPreferencesActivity.class));
     }
 
     void setStereo(boolean stereo) {
@@ -205,79 +207,114 @@ public class UnityARPlayerActivity extends UnityPlayerNativeActivity {
 }
 
 /*
-// FOR REFERENCE, PARENT UNITY3DPLAYER CLASS FOLLOWS.
+// FOR REFERENCE, ORIGINAL UNITY PLAYER ACTIVITY (UnityPlayerNativeActivity is now obsolete)
 
-package com.unity3d.player;
+package com.companyname.product;
 
-import android.app.NativeActivity;
+import com.unity3d.player.*;
+import android.app.Activity;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.graphics.PixelFormat;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
-public class UnityPlayerNativeActivity extends NativeActivity
+public class UnityPlayerActivity extends Activity
 {
-	protected UnityPlayer mUnityPlayer;		// don't change the name of this variable; referenced from native code
+    protected UnityPlayer mUnityPlayer; // don't change the name of this variable; referenced from native code
 
-	// UnityPlayer.init() should be called before attaching the view to a layout - it will load the native code.
-	// UnityPlayer.quit() should be the last thing called - it will unload the native code.
-	protected void onCreate (Bundle savedInstanceState)
-	{
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		super.onCreate(savedInstanceState);
-		
-		getWindow().takeSurface(null);
-		setTheme(android.R.style.Theme_NoTitleBar_Fullscreen);
-		getWindow().setFormat(PixelFormat.RGB_565);
+    // Setup activity layout
+    @Override protected void onCreate (Bundle savedInstanceState)
+    {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
 
-		mUnityPlayer = new UnityPlayer(this);
-		if (mUnityPlayer.getSettings ().getBoolean ("hide_status_bar", true))
-			getWindow ().setFlags (WindowManager.LayoutParams.FLAG_FULLSCREEN,
-			                       WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFormat(PixelFormat.RGBX_8888); // <--- This makes xperia play happy
 
-		int glesMode = mUnityPlayer.getSettings().getInt("gles_mode", 1);
-		boolean trueColor8888 = false;
-		mUnityPlayer.init(glesMode, trueColor8888);
+        mUnityPlayer = new UnityPlayer(this);
+        setContentView(mUnityPlayer);
+        mUnityPlayer.requestFocus();
+    }
 
-		View playerView = mUnityPlayer.getView();
-		setContentView(playerView);
-		playerView.requestFocus();
-	}
-	protected void onDestroy ()
-	{
-		mUnityPlayer.quit();
-		super.onDestroy();
-	}
+    @Override protected void onNewIntent(Intent intent)
+    {
+        // To support deep linking, we need to make sure that the client can get access to
+        // the last sent intent. The clients access this through a JNI api that allows them
+        // to get the intent set on launch. To update that after launch we have to manually
+        // replace the intent with the one caught here.
+        setIntent(intent);
+    }
 
-	// onPause()/onResume() must be sent to UnityPlayer to enable pause and resource recreation on resume.
-	protected void onPause()
-	{
-		super.onPause();
-		mUnityPlayer.pause();
-	}
-	protected void onResume()
-	{
-		super.onResume();
-		mUnityPlayer.resume();
-	}
-	public void onConfigurationChanged(Configuration newConfig)
-	{
-		super.onConfigurationChanged(newConfig);
-		mUnityPlayer.configurationChanged(newConfig);
-	}
-	public void onWindowFocusChanged(boolean hasFocus)
-	{
-		super.onWindowFocusChanged(hasFocus);
-		mUnityPlayer.windowFocusChanged(hasFocus);
-	}
-	public boolean dispatchKeyEvent(KeyEvent event)
-	{
-		if (event.getAction() == KeyEvent.ACTION_MULTIPLE)
-			return mUnityPlayer.onKeyMultiple(event.getKeyCode(), event.getRepeatCount(), event);
-		return super.dispatchKeyEvent(event);
-	}
+    // Quit Unity
+    @Override protected void onDestroy ()
+    {
+        mUnityPlayer.quit();
+        super.onDestroy();
+    }
+
+    // Pause Unity
+    @Override protected void onPause()
+    {
+        super.onPause();
+        mUnityPlayer.pause();
+    }
+
+    // Resume Unity
+    @Override protected void onResume()
+    {
+        super.onResume();
+        mUnityPlayer.resume();
+    }
+
+    // Low Memory Unity
+    @Override public void onLowMemory()
+    {
+        super.onLowMemory();
+        mUnityPlayer.lowMemory();
+    }
+
+    // Trim Memory Unity
+    @Override public void onTrimMemory(int level)
+    {
+        super.onTrimMemory(level);
+        if (level == TRIM_MEMORY_RUNNING_CRITICAL)
+        {
+            mUnityPlayer.lowMemory();
+        }
+    }
+
+    // This ensures the layout will be correct.
+    @Override public void onConfigurationChanged(Configuration newConfig)
+    {
+        super.onConfigurationChanged(newConfig);
+        mUnityPlayer.configurationChanged(newConfig);
+    }
+
+    // Notify Unity of the focus change.
+    @Override public void onWindowFocusChanged(boolean hasFocus)
+    {
+        super.onWindowFocusChanged(hasFocus);
+        mUnityPlayer.windowFocusChanged(hasFocus);
+    }
+
+    // For some reason the multiple keyevent type is not supported by the ndk.
+    // Force event injection by overriding dispatchKeyEvent().
+    @Override public boolean dispatchKeyEvent(KeyEvent event)
+    {
+        if (event.getAction() == KeyEvent.ACTION_MULTIPLE)
+            return mUnityPlayer.injectEvent(event);
+        return super.dispatchKeyEvent(event);
+    }
+
+    // Pass any events not handled by (unfocused) views straight to UnityPlayer
+    @Override public boolean onKeyUp(int keyCode, KeyEvent event)     { return mUnityPlayer.injectEvent(event); }
+    @Override public boolean onKeyDown(int keyCode, KeyEvent event)   { return mUnityPlayer.injectEvent(event); }
+    @Override public boolean onTouchEvent(MotionEvent event)          { return mUnityPlayer.injectEvent(event); }
+    //API12
+    public boolean onGenericMotionEvent(MotionEvent event)  { return mUnityPlayer.injectEvent(event); }
 }
 */
